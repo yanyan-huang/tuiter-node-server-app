0e6aa21ac3ea5d38deaa03fd2f8c79250cd8f9b1
yanyan-huang <huang.yanya@northeastern.edu>
2023-04-07 23:22:32 -0700

    fix CONNECTION_STRING

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";// load the mongoose library
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');// connect to the tuiter database
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter' 
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(cors()) // configure cors right after instantiating express; to allow cross-origin requests
